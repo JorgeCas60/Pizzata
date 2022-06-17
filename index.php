b407ae19430b3472e68c0af086e0a7a85927c6a9
Jorge Castillo <jorgecas60>
2022-06-16 20:59:42 -0400

    Base de la pizzata

---
+++ index.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Login - SB Admin</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-body">
+                                        <form action="#" method="POST">
+                                        <?php
+                     include_once 'database.php';
+
+    session_start();
+
+    if(isset($_GET['cerrar_sesion'])){
+        session_unset();
+
+        session_destroy();
+    }
+
+    if(isset($_SESSION['rol'])){
+        switch($_SESSION['rol']){
+            case 1:
+                header('location: admin.php');
+            break;
+
+            case 2:
+            header('location: colab.php');
+            break;
+
+            default:
+        }
+    }
+
+    if(isset($_POST['username']) && isset($_POST['password'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $db = new Database();
+        $query = $db->connect()->prepare('SELECT*FROM usuarios WHERE username = :username AND password = :password');
+        $query->execute(['username' => $username, 'password' => $password]);
+
+        $row = $query->fetch(PDO::FETCH_NUM);
+        if($row == true){
+            // validar rol
+            $rol = $row[3];
+            $_SESSION['rol'] = $rol;
+
+            switch($_SESSION['rol']){
+                case 1:
+                    header('location: admin.php');
+                break;
+    
+                case 2:
+                header('location: colab.php');
+                break;
+    
+                default:
+            }
+        }else{
+            // no existe el usuario
+            echo "El usuario o contraseña son incorrectos";
+        }
+
+    }
+    
+
+?>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" name ="username" id="inputUsername" type="text" placeholder="Username" />
+                                                <label for="inputEmail">Username</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" name="password" id="inputPassword" type="text" placeholder="Password" />
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <input  class=btn-primary type="submit" value="Iniciar sesión">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2022</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
